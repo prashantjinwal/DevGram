e77dd022f6a0a26ea6d5bbd2eb2393210b0b191e
prashantjinwal <prashantjinwal888@gmail.com>
2025-04-22 12:56:37 +0530

    add commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav className="fixed bottom-0 w-full z-50 border-t-1 border-amber-50">
+         <ul className="py-4 px-3 flex justify-between text-white">
+                <ol className="flex justify-between">HOME</ol>
+                <ol className="flex justify-between">HOME</ol>
+                <ol className="flex justify-between">HOME</ol>
+                <ol className="flex justify-between">HOME</ol>
+         </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
