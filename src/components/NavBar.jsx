f2d4d13a9146c6accb0048d5e5f08c631d180e2a
prashantjinwal <prashantjinwal888@gmail.com>
2025-04-23 22:17:05 +0530

    Add Responsivenesssssssssss

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,14 +6,15 @@ import Person2Icon from '@mui/icons-material/Person2';
 
 const NavBar = () => {
   return (
-    <nav className="fixed bottom-0 w-full z-50 border-t-1 border-amber-50">
-         <ul className="py-4 px-3 flex justify-between text-white">
-                <ol className="flex justify-between"><HomeFilledIcon fontSize="large" /></ol>
-                <ol className="flex justify-between"><ExploreIcon fontSize="large" /></ol>
-                <ol className="flex justify-between"><PostAddIcon fontSize="large" /></ol>
-                <ol className="flex justify-between"><Person2Icon fontSize="large" /></ol>
-         </ul>
+    <nav className="fixed bottom-0 z-50 w-full border-t border-amber-50 lg:left-1/2 lg:translate-x-[-50%] lg:w-[50%] lg:border lg:rounded-full px-5">
+      <ul className="py-4 px-3 flex justify-between text-white">
+        <li className="flex justify-between"><HomeFilledIcon fontSize="large" /></li>
+        <li className="flex justify-between"><ExploreIcon fontSize="large" /></li>
+        <li className="flex justify-between"><PostAddIcon fontSize="large" /></li>
+        <li className="flex justify-between"><Person2Icon fontSize="large" /></li>
+      </ul>
     </nav>
+
   );
 };
 
