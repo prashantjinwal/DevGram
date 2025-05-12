834757dc2d5d3b5d15ae1316528cf81cd5862e82
prashantjinwal <prashantjinwal888@gmail.com>
2025-05-12 14:28:10 +0530

    Add index

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,13 @@
-console.log("test command sucessfully run")
+import express from "express"
+const app = express();
+
+app.get('/', (res,res)=>{
+    res.send("server is ready");
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=>{
+    console.log(`server ready at http://localhost:${port}`)
+})
+ 
