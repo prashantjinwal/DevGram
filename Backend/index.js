804f495644751f17cacfc1da292f4f28bc361767
prashantjinwal <prashantjinwal888@gmail.com>
2025-05-12 15:27:52 +0530

    add proxy

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ app.get('/', (res,res)=>{
     res.send("server is ready");
 })
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 app.listen(port, ()=>{
     console.log(`server ready at http://localhost:${port}`)
