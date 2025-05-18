fa103d0085073a55489201aef4411d7ebf4fa5cc
prashantjinwal <prashantjinwal888@gmail.com>
2025-05-18 22:24:29 +0530

    Add update UI

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import Person2Icon from '@mui/icons-material/Person2';
 
 const NavBar = () => {
   return (
-    <nav className="fixed bottom-0 z-50 w-full border-t border-amber-50 lg:left-1/2 lg:translate-x-[-50%] lg:w-[50%] lg:border lg:rounded-full px-5">
+    <nav className="fixed bottom-0 z-50 w-full border-t border-amber-50 lg:left-1/2 lg:translate-x-[-50%] lg:w-[50%] lg:border lg:rounded-full px-5 cursor-pointer">
       <ul className="py-4 px-3 flex justify-between text-white">
         <li className="flex justify-between"><HomeFilledIcon fontSize="large" /></li>
         <li className="flex justify-between"><ExploreIcon fontSize="large" /></li>
