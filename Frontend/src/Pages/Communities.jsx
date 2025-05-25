7f601e1570a37ccf13a0a02069108a213f013b5e
Prashant Jinwal <98511988+prashantjinwal@users.noreply.github.com>
2025-05-25 19:06:58 +0530

    Update Communities.jsx

--- Frontend/src/Pages/Communities.jsx
+++ Frontend/src/Pages/Communities.jsx
@@ -1,7 +1,7 @@
 export const Communities = () =>{
     return(
         <>
-            <h1 className="text-2xl text-amber-50">Communities</h1>
+            <h1 className="text-2xl text-amber-50">Communities..</h1>
         
         </>
     );
