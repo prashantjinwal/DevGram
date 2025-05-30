9348e744d3191efecdb2fcdab514d975eb027be5
prashantjinwal <prashantjinwal888@gmail.com>
2025-05-30 16:25:18 +0530

    Add profile fix

--- Frontend/src/Pages/Profile.jsx
+++ Frontend/src/Pages/Profile.jsx
@@ -1,17 +1,44 @@
-// import { useState } from "react";
+import { useState } from "react";
 import { Profile_H } from "../components/Profile/Profile_H";
 import { ProjectBar } from "../components/Profile/ProjectBar";
+import ResponsiveButton from "../components/buttons/ReusebleButton";
 
 export const Profile = () =>{
-    // const [logged, setlogged] = useState(false)
+    const [logged, setlogged] = useState(false)
     return(
-        <div>
-            {/* <h1 className="text-2xl text-amber-50">Profile</h1> */}
-            <Profile_H/>
-            {/* Project bar */}
-            <ProjectBar/>
+        <>
+            {
+                logged ?
 
-        
+         (
+            <>
+                <Profile_H/>
+                <ProjectBar/>
+            </>
+        ) :
+        <div className="flex items-center justify-center h-screen">
+            <div className="flex gap-4 w-[50%]">
+                <ResponsiveButton
+                  label="Login"
+                  onClick={() => console.log("Edit Profile")}
+                  bgColor="bg-white"
+                  textColor="text-black"
+                />
+
+                <ResponsiveButton
+                  label="Sign up"
+                  onClick={() => console.log("Share Profile")}
+                  bgColor="bg-transparent"
+                  textColor="text-white"
+                  border="border-2 border-amber-50"
+                />
+            </div>
         </div>
+
+
+
+            }
+        
+        </>
     );
 }
