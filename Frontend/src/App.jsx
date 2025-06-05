710dcab60bc68e7a6c6c5c14c4965cc162f758e9
prashantjinwal <prashantjinwal888@gmail.com>
2025-06-05 20:28:24 +0530

    Add login redirect

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,8 +4,7 @@ import LoginPage from './signUp/LoginPage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './PageHandler/Dashboard_t.jsx';
 import SignUp from './signUp/SignUp';
-// import { SideBar } from './components/Navigator/SideBar';
-// import { Home } from './Pages/Home';
+
 
 
 
@@ -19,8 +18,8 @@ function App() {
       {/* dashboard/* */}
       <Routes>
         <Route path="/*" element={<Dashboard />} />
-        {/* <Route path="/" element={<LoginPage/>} /> 
-        <Route path="/signup" element={<SignUp/>} /> */}
+        <Route path="/" element={<LoginPage/>} /> 
+        <Route path="/signup" element={<SignUp/>} />
       </Routes>
       {/* <SideBar/>
       <Home/> */}
--- Frontend/src/Pages/Profile.jsx
+++ Frontend/src/Pages/Profile.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Profile_H } from "../components/Profile/Profile_H";
 import { ProjectBar } from "../components/Profile/ProjectBar";
 import ResponsiveButton from "../components/buttons/ReusebleButton";
+import { Link } from "react-router";
 
 export const Profile = () =>{
     const [logged, setlogged] = useState(false)
@@ -16,24 +17,23 @@ export const Profile = () =>{
                 <ProjectBar/>
             </>
         ) :
-        <div className="flex items-center justify-center h-screen">
-            <div className="flex gap-4 w-[50%]">
-                <ResponsiveButton
-                  label="Login"
-                  onClick={() => console.log("Edit Profile")}
-                  bgColor="bg-white"
-                  textColor="text-black"
-                />
+       <div className="flex items-center justify-center h-screen px-4">
+        <div className="flex flex-col md:flex-row gap-4 w-full md:w-1/2">
+    
+            <Link to="/" className="w-full">
+                <button className="w-full px-6 py-3 bg-white text-black font-semibold rounded-xl shadow-lg hover:bg-gray-100 hover:scale-105 active:scale-95 transition-all duration-300 text-base md:text-lg">
+                    Login
+                </button>
+            </Link>
 
-                <ResponsiveButton
-                  label="Sign up"
-                  onClick={() => console.log("Share Profile")}
-                  bgColor="bg-transparent"
-                  textColor="text-white"
-                  border="border-2 border-amber-50"
-                />
-            </div>
-        </div>
+            <Link to="/signup" className="w-full">
+                <button className="w-full px-6 py-3 bg-black text-white font-semibold rounded-xl shadow-lg hover:bg-gray-900 hover:scale-105 active:scale-95 transition-all duration-300 text-base md:text-lg">
+                    Sign Up
+                </button>
+            </Link>
+
+       </div>
+     </div>
 
 
 
